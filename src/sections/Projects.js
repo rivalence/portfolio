38fdf6f8c1f09677efdc26899db1e73acc6df3e6
Rick Ndelo <valenceanotho10@gmail.com>
2023-07-18 00:56:15 +0200

    final responsive

--- src/sections/Projects.js
+++ src/sections/Projects.js
@@ -6,9 +6,9 @@ import demoFamille from '../assets/videos/Demo famille.mp4'
 
 function Projects() {
   return (
-    <div className='w-full md:w-10/12 lg:w-9/12 pt-16 flex justify-center'>
-        <div className='border border-black rounded-lg p-8 bg-orange-100'>
-            <h1 className='font-medium'>Mes projets</h1><hr className='border-gray-500 pb-8' />
+    <div className='w-full md:w-11/12 lg:w-11/12 pt-8 flex justify-center'>
+        <div className='border border-black rounded-lg p-4 bg-orange-100'>
+            <h1 className='font-medium'>Mes projets</h1><hr className='border-gray-500 pb-4' />
             <div>
                 <p>
                     <IconsFade icon={faArrowTurnRight}/> Feecare: Réseau social privé proposant en plus, une gestion de 
@@ -17,13 +17,13 @@ function Projects() {
                     jour des créneaux de disponibilité). De l'autre, on a des comptes de familles qui peuvent réagir aux posts 
                     (liker, commenter) et prendre RDV avec leurs éducateurs, en fonction des 
                     disponibilités de ceux-ci. 
-                    <br/>•Cette application, développée en Symfony/PostGreSQL côté back-end et javaScript 
+                    <br/><br/>• Cette application, développée en Symfony/PostGreSQL côté back-end et javaScript 
                     natif/BootStrap côté front-end, était pour le compte d'un projet de fin de cours en faculté.
                     Le projet n'est pas hébergé en ligne. Explorez les démos des deux interfaces ci-dessous ou accédez au code source 
                     sur mon github : <a className='hover:text-sky-700' href='https://github.com/rivalence/feecare'>https://github.com/rivalence/feecare</a>
                 </p>
 
-                <div className='flex flex-row gap-x-16 justify-center'>
+                <div className='flex flex-row gap-x-4 justify-center'>
                     <div>
                         <p className='pt-8 font-medium pb-4'><Icons icon={faVideo} /> Interface Educateur</p>
                         <video controls width="400" height="250" src={demoEducateur}>
@@ -44,8 +44,8 @@ function Projects() {
                 <p>
                     <IconsFade icon={faArrowTurnRight}/> Codexia : Jeu mobile d'énigmes (Android et iOS), en cours de développement. Il 
                     sera très prochainement disponible.
-                    <br/>•Stack technique: React Native, MongoDB.
-                    <br/>•Accédez au repositorie sur Github:  <a className='hover:text-sky-700' href='https://github.com/rivalence/codexia'>
+                    <br/><br/>• Stack technique: React Native, MongoDB.
+                    <br/>• Accédez au repositorie sur Github:  <a className='hover:text-sky-700' href='https://github.com/rivalence/codexia'>
                         https://github.com/rivalence/codexia</a>
                 </p>
             </div>    
