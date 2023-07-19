d333da4f68a3550448c1d3fd6e3ff80d58da8573
Rick Ndelo <valenceanotho10@gmail.com>
2023-07-19 16:36:53 +0200

    menu de navigation rajoutés

--- src/sections/AboutMe.js
+++ src/sections/AboutMe.js
@@ -1,32 +1,57 @@
-import photo2 from '../assets/images/Ma photo 2.jpg'
 import libraryIcon from '../assets/icones/icons8-bibliothèque-64.png'
 import footballIcon from '../assets/icones/icons8-football-94.png'
 import guitaristIcon from '../assets/icones/icons8-guitarist-96.png'
+import { useState } from 'react'
+import NavigationButton from '../components/NavigationButton'
 
 export default function AboutMe() {
-    return (
-        <div className='flex flex-row pt-6 gap-x-4 md:gap-x-6 lg:gap-x-8 w-full md:w-11/12 lg:w-11/12'>
-            <div className='flex items-center'>
-                <img 
-                    className='rounded-3xl w-30 h-40 md:w-40 md:h-55 lg:w-64 lg:h-64' 
-                    src={photo2} 
-                    alt='Rick NDELO'
-                
-                />
-            </div>
+    const [selfShow, setSelfShow] = useState(true)
+    const [interetShow, setInteretShow] = useState(false)
 
-            <div className='w-10/12 border border-black bg-orange-100 p-4 rounded-lg'>
+    const displaySelf = () => {
+        setSelfShow(true)
+        setInteretShow(false)
+    }
+
+    const displayInteret = () => {
+        setInteretShow(true)
+        setSelfShow(false)
+    }
+
+    return (
+        <div className='flex justify-center pt-6 w-full md:w-11/12'>
+            <div className='border border-black bg-orange-100 p-4 rounded-lg'>
                 <h1>Qui suis-je ?</h1><hr className='border-gray-400'/>
-                <p className='pt-4'>
-                    Je suis Rick NDELO. Je me forme actuellement à la 3W Academy afin d'obtenir la qualification de 
-                    Concepteur Développeur d'Applications. A cet effet, je recherche une entreprise d'accueil, dans le département
-                    des Alpes Maritimes, pour une alternance de 12 mois avec un rythme de 3 semaines en entreprise et 1 semaine en 
-                    cours.
-                </p><br />
-                <h2 className='text-center'>CENTRES D'INTERÊT</h2>
+
+                {/* Container navigation */}
+                <NavigationButton 
+                    buttons={[
+                        {
+                            name: "Self",
+                            method : displaySelf,
+                            state : selfShow
+                        },
+                        {
+                            name: "Centres d'intérêts",
+                            method: displayInteret,
+                            state: interetShow
+                        }
+                    ]}
+                />
+
+                {/* Self content  */}
+                <div className={selfShow ? "block p-4" : "hidden"}>
+                    <p className='pt-4'>
+                        Je suis Rick NDELO. Je me forme actuellement à la 3W Academy afin d'obtenir la qualification de 
+                        Concepteur Développeur d'Applications. A cet effet, je recherche une entreprise d'accueil, dans le département
+                        des Alpes Maritimes, pour une alternance de 12 mois avec un rythme de 3 semaines en entreprise et 1 semaine en 
+                        cours.
+                    </p>
+                </div>
+                
 
                 {/* Cadre de centres d'intérets */}
-                <div className='flex flex-col items-center gap-y-4'>
+                <div className={interetShow ? "flex flex-row items-center gap-x-4 block p-4" : "hidden"}>
                     <div className='border border-gray-400 rounded p-2'>
                         <div className='flex justify-center'><img src={libraryIcon} alt='Library' className='w-15 h-10'/></div>
                         <div className='mt-4'>
